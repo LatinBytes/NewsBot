9cd5f88bfcf2cf0562cdbb6e37f2e98f01be79a7
Tower95 <torrestmi3@gmail.com>
2021-01-08 17:22:26 -0700

    Commands

--- src/components/bot/commands.js
+++ src/components/bot/commands.js
@@ -1,16 +1,14 @@
 const { client } = require('../Cliente')
-function getCommand(msg){
+function getChannel(msg){
 
 
     let command = msg.split(' ');
     let key = command[0];
     let channel = command[1].slice(2,command[1].length -1);
-    // console.log(command.join());
-    console.log(command);
-    console.log(channel);
+    
     return channel;
 }
 
 module.exports  ={
-    getCommand
+    getChannel
 }
--- src/components/bot/message.js
+++ src/components/bot/message.js
@@ -1,17 +1,15 @@
 
 const { client } = require('../Cliente')
-const {getCommand} = require('../bot/commands')
+const {getChannel} = require('../bot/commands')
 
 let AsigmentChannel = () => {
   client.on('message', msg => {
-    switch(msg.content){
-      case '$news':{msg.reply(`Este es el canal <#${channel}>`);}
-      break;
-      default:break;
-    }
     if(msg.content.startsWith('$news')){
-      let chanel = getCommand(msg.content);
-    }
+    
+    let channel = getChannel(msg.content);
+    msg.reply(`Este es el canal <#${channel}>`);
+    console.log(channel);
+  }
   });   
 }
 module.exports = {
