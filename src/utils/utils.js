c613f5687f736c531ae913eec77dcc63e2e6cacc
Joshua Sacuedo <joshuansu0897@gmail.com>
2021-01-16 22:08:59 -0500

    utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,21 @@
 exports.trim = (string) => {
   return string.replace(/\s\s+|\n/g, '')
 }
+
+exports.split = (string) => {
+  return string.split(/[ ,]+/)
+}
+
+exports.clearDiscordID = (id) => {
+  return id.replace(/\D/g, '')
+}
+
+exports.arrayToStringList = (arr) => {
+  let response = ''
+
+  arr.forEach(item => {
+    response = response + `\t- ${item}\n`
+  })
+
+  return response
+}
